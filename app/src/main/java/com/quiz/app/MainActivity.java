a0d860676362014c9a3b198f81c806a887cafcb5
cvxiwang <809749956@qq.com>
2026-02-28 19:53:40 +0800

    修改路径测试

--- app/src/main/java/com/quiz/app/MainActivity.java
+++ app/src/main/java/com/quiz/app/MainActivity.java
@@ -1,13 +1,22 @@
 package com.quiz.app;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.webkit.ConsoleMessage;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebResourceResponse;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.webkit.WebViewAssetLoader;
 
 public class MainActivity extends AppCompatActivity {
     private WebView webView;
+    private WebViewAssetLoader assetLoader;
+    private static final String TAG = "QuizApp";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +24,12 @@ public class MainActivity extends AppCompatActivity {
         
         webView = new WebView(this);
         setContentView(webView);
+        
+        // 创建 WebViewAssetLoader - 将本地资源通过 https://appassets.androidplatform.net/ 提供
+        assetLoader = new WebViewAssetLoader.Builder()
+                .addPathHandler("/assets/", new WebViewAssetLoader.AssetsPathHandler(this))
+                .addPathHandler("/res/", new WebViewAssetLoader.ResourcesPathHandler(this))
+                .build();
         
         WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
@@ -31,21 +46,38 @@ public class MainActivity extends AppCompatActivity {
         // 允许混合内容
         webSettings.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
         
-        // 允许从本地文件加载内容
-        webSettings.setAllowFileAccessFromFileURLs(true);
-        webSettings.setAllowUniversalAccessFromFileURLs(true);
-        
         // 设置 WebViewClient
         webView.setWebViewClient(new WebViewClient() {
             @Override
-            public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                view.loadUrl(url);
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                return false;
+            }
+            
+            @Override
+            public WebResourceResponse shouldInterceptRequest(WebView view, WebResourceRequest request) {
+                return assetLoader.shouldInterceptRequest(request.getUrl());
+            }
+            
+            @Override
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                Log.e(TAG, "WebView Error: " + description + " URL: " + failingUrl);
+                Toast.makeText(MainActivity.this, "加载错误: " + description, Toast.LENGTH_LONG).show();
+            }
+        });
+        
+        // 设置 WebChromeClient 捕获控制台日志
+        webView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public boolean onConsoleMessage(ConsoleMessage consoleMessage) {
+                Log.d(TAG, "Console [" + consoleMessage.messageLevel() + "]: " + consoleMessage.message());
                 return true;
             }
         });
         
-        // 加载本地 HTML 文件
-        webView.loadUrl("file:///android_asset/index.html");
+        // 使用 WebViewAssetLoader 的 URL 加载
+        String url = "https://appassets.androidplatform.net/assets/index.html";
+        Log.d(TAG, "Loading: " + url);
+        webView.loadUrl(url);
     }
 
     @Override
